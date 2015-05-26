6282f7cc0ef8a1d5be205d711caaaad323898152
seas521 <seas521@163.com>
2015-05-26 15:44:38 +0800

    将jsp改为velocity

--- springmvc/src/main/java/cn/springmvc/controller/UserController.java
+++ springmvc/src/main/java/cn/springmvc/controller/UserController.java
@@ -1,15 +1,35 @@
 package cn.springmvc.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
 	
+	private Logger logger = Logger.getLogger(UserController.class);
+	
+	private String username;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	@RequestMapping("index")
-	public String index(){
-		return "index";
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping("login")
+	public ModelAndView login(){
+		logger.debug("Test=========");
+		return new ModelAndView("login");
 	}
 
 }
